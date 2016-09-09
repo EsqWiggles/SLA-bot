840efdbc3771f60881e4052feaea18a9ea7d8eda
Wiggly <esqwiggles@gmail.com>
2016-09-09 00:09:13 -0700

    Add functions to get external alerts
    For the purpose of getting unscheduled events

--- SLA_bot/gameevent.py
+++ SLA_bot/gameevent.py
@@ -24,3 +24,31 @@ class GameEvent:
     def __repr__(self):
         return 'GameEvent({}, {}, {})'.format(self.name, self.start, self.end)
 
+
+class MultiShipEvent(GameEvent):
+    def __init__(self, name, ships, start, end = None):
+        super().__init__(name, start, end)
+        self.ships = ships
+        self.unscheduled = False
+        for event in self.ships[1:]:
+            if event:
+                self.unscheduled = True
+
+    def multi_dur(self, targets, tz):
+        if self.unscheduled == False:
+            return self.ships[0]
+        
+        ship_events = []
+        for index in targets:
+            line = '`ship {:02d}: `{}'.format(index, self.ships[index])
+            ship_events.append(line)
+        if len(ship_events) < 1:
+            return ''
+        
+        header = self.duration(tz)
+        body = '\n'.join(ship_events)
+        return '{}\n{}'.format(header, body)
+
+    def __repr__(self):
+        return 'MultiShipEvent({}, {}, {}, {})'.format(self.name, self.ships,
+                                                       self.start, self.end)
