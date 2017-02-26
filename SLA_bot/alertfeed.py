531a030f4b06c6dc49abe79ae43b5eb4cef2dd76
Wiggly <esqwiggles@gmail.com>
2017-02-26 02:11:14 -0800

    Disable all old systems and implement simple new EQ feed updater

--- SLA_bot/alertfeed.py
+++ SLA_bot/alertfeed.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime as dt
+import json
 import re
 
 import aiohttp
@@ -7,49 +8,70 @@ import pytz
 
 from   SLA_bot.gameevent import MultiShipEvent
 
+class AlertFeed:
+    source_url = 'http://pso2emq.flyergo.eu/api/v2/'
 
-async def download(url):
-    async with aiohttp.get(url) as response:
-        return await response.json()
+    async def download(url):
+        try:
+            async with aiohttp.get(url) as response:
+                return await response.json()
+        except json.decoder.JSONDecodeError:
+            pass
+        except Exception:
+            print('Ignored following error:')
+            print(traceback.format_exc(), file=sys.stderr)
 
-def parse_event_text(text):
-    events = [''] * 11
-    events[0] = text
-    data = text.split('\n')
-    for i in range(1,11):
-        ship = '{:02d}:'.format(i)
-        regex = '{}(?!\d\d)'.format(ship)
-        for line in data:
-            line = line.strip()
-            if re.match(regex, line):
-                events[i] = line[3:]
-    return events
+    def parse_data(data):
+        latest_alert = data[0]['text']
+        lines = latest_alert.splitlines()
+        header = '-' * len(lines[0])
+        lines.insert(1, header)
+        text = '\n'.join(lines)
+        return '```fix\n{}\n```'.format(text)
+            
+    async def fetch():
+        raw_data = await AlertFeed.download(AlertFeed.source_url)
+        return AlertFeed.parse_data(raw_data)
+        
 
-def parse_notices(notices, ref_dt):
-    if notices == None or len(notices) < 1:
-        return []
+# def parse_event_text(text):
+    # events = [''] * 11
+    # events[0] = text
+    # data = text.split('\n')
+    # for i in range(1,11):
+        # ship = '{:02d}:'.format(i)
+        # regex = '{}(?!\d\d)'.format(ship)
+        # for line in data:
+            # line = line.strip()
+            # if re.match(regex, line):
+                # events[i] = line[3:]
+    # return events
 
-    parsed = []
-    jst = pytz.timezone('Asia/Tokyo')
-    title = 'Unscheduled Emergency Quest'
+# def parse_notices(notices, ref_dt):
+    # if notices == None or len(notices) < 1:
+        # return []
+
+    # parsed = []
+    # jst = pytz.timezone('Asia/Tokyo')
+    # title = 'Unscheduled Emergency Quest'
     
-    base_dt = ref_dt.astimezone(jst)
-    base_date = base_dt.date()
-    if notices[0]['jst'] == 0 and base_dt.hour >= 22:
-        base_date += dt.timedelta(days=1)
+    # base_dt = ref_dt.astimezone(jst)
+    # base_date = base_dt.date()
+    # if notices[0]['jst'] == 0 and base_dt.hour >= 22:
+        # base_date += dt.timedelta(days=1)
 
-    prev_hour = 99
-    for n in notices:
-        hour = n['jst']
-        guess_dt = dt.datetime.combine(base_date, dt.time(hour=hour))
-        guess_dt = jst.localize(guess_dt)
-        guess_dt = guess_dt.astimezone(dt.timezone.utc)
-        events = parse_event_text(n['text'])
+    # prev_hour = 99
+    # for n in notices:
+        # hour = n['jst']
+        # guess_dt = dt.datetime.combine(base_date, dt.time(hour=hour))
+        # guess_dt = jst.localize(guess_dt)
+        # guess_dt = guess_dt.astimezone(dt.timezone.utc)
+        # events = parse_event_text(n['text'])
         
-        parsed.append(MultiShipEvent(title, events, guess_dt))
+        # parsed.append(MultiShipEvent(title, events, guess_dt))
         
-        if prev_hour <  hour:
-            base_date -= dt.timedelta(days=1)
-        prev_hour = hour
-    return parsed
+        # if prev_hour <  hour:
+            # base_date -= dt.timedelta(days=1)
+        # prev_hour = hour
+    # return parsed
         
--- sla_bot.py
+++ sla_bot.py
@@ -15,6 +15,7 @@ from   SLA_bot.config import Config as cf
 import SLA_bot.constants as cs
 from   SLA_bot.schedule import Schedule
 import SLA_bot.util as ut
+from   SLA_bot.alertfeed import AlertFeed
 
 VERSION = 0.17
 
@@ -32,96 +33,158 @@ cf.load_config(configs)
 
 bot = commands.Bot(command_prefix=cf.cmd_prefix, pm_help = True,
                    description=cs.BOT_HELP)
-event_schedule = Schedule(bot)
-bot.add_cog(event_schedule)
+channel_messages = {}
+                   
+class Cabinet:
+    module_cache = collections.OrderedDict()
+    module_cache[AlertFeed] = 'No data'
 
-async def update_schedule():
-    while not bot.is_closed:
-        try:
-            await event_schedule.update()
-        except Exception:
-            print('Ignored following error:')
-            print(traceback.format_exc(), file=sys.stderr)
-        await asyncio.sleep(cf.refresh_time)
+    async def update():
+        for m in Cabinet.module_cache:
+            Cabinet.module_cache[m] = await m.fetch()
+            
+    def contents():
+        components = []
+        for k, v in Cabinet.module_cache.items():
+            components.append(v)
+        return components
+            
 
-bot.loop.create_task(update_schedule())
-
-def country_zone():
-    original = pytz.country_timezones
-    inverted = {}
-    for k, v in original.items():
-        for zone in v:
-            inverted[zone] = pytz.country_names[k]
-    return inverted
-    
-async def clock(bot):
-    while not bot.is_closed:
-        now = dt.datetime.now(dt.timezone.utc)
-        try:
-            time = now.astimezone(cf.tz).strftime('%H:%M %Z')
-            help = 'Type {}help'.format(cf.cmd_prefix)
-            status = '{} - {}'.format(time, help)
-            await bot.change_status(game=discord.Game(name=status))
-        except Exception:
-            print('Ignored following error:')
-            print(traceback.format_exc(), file=sys.stderr)
-        await asyncio.sleep(60 - now.second)
-
-@bot.command(help = cs.TZLIST_HELP)
-async def tzlist(mode='other'):
-    countries = collections.OrderedDict()
-    others = collections.OrderedDict()
-    country_codes = {x.lower() for x in pytz.country_timezones.keys()} 
-    cc_from_tz = country_zone()
-    for k, v in cf.custom_tz.items():
-        if k in country_codes:
-            countries[k] = v
+async def recyle_messages(channel):
+    required_messages = len(Cabinet.module_cache)
+    messages = bot.logs_from(channel, limit=required_messages)
+    recyled = []
+    async for msg in messages:
+        if msg.author.id == bot.user.id:
+            recyled.append(msg)
         else:
-            others[k] = v
-
-    if mode == 'other':
-        selected = others
-    elif mode == 'country':
-        selected = countries
-    else:
-        selected = collections.OrderedDict()
-
-    lines = []
-    for k, v in selected.items():
-        country = cc_from_tz[v] if v in cc_from_tz else 'No Country'
-        line = '{} = {} - {}'.format(k, v, country)
-        lines.append(line)
-    msg = ut.chunk_string('\n'.join(lines), cs.MSG_CHAR_LIMIT)
-    await ut.quiet_say(bot, msg, cf.max_line)
-
-@bot.command(help = cs.TZINFO_HELP)
-async def tzinfo(timezone=''):
-    tz = ut.parse_tz(timezone, cf.tz, cf.custom_tz)
-    
-    country = 'No Country'
-    area = getattr(tz, 'zone', 'Custom time zone')
-    if hasattr(tz, 'zone'):
-        for k, v in pytz.country_timezones.items():
-            if tz.zone in v:
-                country = pytz.country_names[k]
+            break
+    return recyled
+            
+async def write(chan_msgs):
+    texts = Cabinet.contents()
+    required_messages = len(Cabinet.module_cache)
+    for chan, msgs in chan_msgs.items():
+        if chan == None:
+            continue
+            
+        for i in range(required_messages):
+            try:
+                msgs[i] = await bot.edit_message(msgs[i], texts[i])
+            except IndexError:
+                new_msg = await bot.send_message(chan, texts[i])
+                msgs.append(new_msg)
+            except discord.errors.NotFound:
+                chan_msgs[chan] = await recyle_messages(chan)
                 break
+        
+async def updater():
+    while not bot.is_closed:
+        try:
+            await Cabinet.update()
+            await write(channel_messages)
+        except Exception:
+            print('Ignored following error:')
+            print(traceback.format_exc(), file=sys.stderr)
+
+        await asyncio.sleep(20)
+
+
+
+# event_schedule = Schedule(bot)
+# bot.add_cog(event_schedule)
+
+# async def update_schedule():
+    # while not bot.is_closed:
+        # try:
+            # await event_schedule.update()
+        # except Exception:
+            # print('Ignored following error:')
+            # print(traceback.format_exc(), file=sys.stderr)
+        # await asyncio.sleep(cf.refresh_time)
+
+# bot.loop.create_task(update_schedule())
+
+# def country_zone():
+    # original = pytz.country_timezones
+    # inverted = {}
+    # for k, v in original.items():
+        # for zone in v:
+            # inverted[zone] = pytz.country_names[k]
+    # return inverted
     
-    dt_line = dt.datetime.now(tz).strftime('%a %b %m, %H:%M %Z (UTC%z)')
-    msg = '{} = {} - {}\n{}'.format(timezone, area, country, dt_line)
+# async def clock(bot):
+    # while not bot.is_closed:
+        # now = dt.datetime.now(dt.timezone.utc)
+        # try:
+            # time = now.astimezone(cf.tz).strftime('%H:%M %Z')
+            # help = 'Type {}help'.format(cf.cmd_prefix)
+            # status = '{} - {}'.format(time, help)
+            # await bot.change_status(game=discord.Game(name=status))
+        # except Exception:
+            # print('Ignored following error:')
+            # print(traceback.format_exc(), file=sys.stderr)
+        # await asyncio.sleep(60 - now.second)
+
+# @bot.command(help = cs.TZLIST_HELP)
+# async def tzlist(mode='other'):
+    # countries = collections.OrderedDict()
+    # others = collections.OrderedDict()
+    # country_codes = {x.lower() for x in pytz.country_timezones.keys()} 
+    # cc_from_tz = country_zone()
+    # for k, v in cf.custom_tz.items():
+        # if k in country_codes:
+            # countries[k] = v
+        # else:
+            # others[k] = v
+
+    # if mode == 'other':
+        # selected = others
+    # elif mode == 'country':
+        # selected = countries
+    # else:
+        # selected = collections.OrderedDict()
+
+    # lines = []
+    # for k, v in selected.items():
+        # country = cc_from_tz[v] if v in cc_from_tz else 'No Country'
+        # line = '{} = {} - {}'.format(k, v, country)
+        # lines.append(line)
+    # msg = ut.chunk_string('\n'.join(lines), cs.MSG_CHAR_LIMIT)
+    # await ut.quiet_say(bot, msg, cf.max_line)
+
+# @bot.command(help = cs.TZINFO_HELP)
+# async def tzinfo(timezone=''):
+    # tz = ut.parse_tz(timezone, cf.tz, cf.custom_tz)
     
-    await ut.quiet_say(bot, msg, cf.max_line)
+    # country = 'No Country'
+    # area = getattr(tz, 'zone', 'Custom time zone')
+    # if hasattr(tz, 'zone'):
+        # for k, v in pytz.country_timezones.items():
+            # if tz.zone in v:
+                # country = pytz.country_names[k]
+                # break
+    
+    # dt_line = dt.datetime.now(tz).strftime('%a %b %m, %H:%M %Z (UTC%z)')
+    # msg = '{} = {} - {}\n{}'.format(timezone, area, country, dt_line)
+    
+    # await ut.quiet_say(bot, msg, cf.max_line)
     
 @bot.event
 async def on_ready():
     print('Logged in as: {}'.format(bot.user.name))
     print('------')
-    alerter = AlertSystem(bot, event_schedule)
-    bot.add_cog(alerter)
-    bot.loop.create_task(alerter.feed_update())
-    bot.loop.create_task(alerter.update())
-    bot.loop.create_task(clock(bot))
+    # alerter = AlertSystem(bot, event_schedule)
+    # bot.add_cog(alerter)
+    # bot.loop.create_task(alerter.feed_update())
+    # bot.loop.create_task(alerter.update())
+    # bot.loop.create_task(clock(bot))
+    for c in cf.channels:
+        channel = bot.get_channel(c[0])
+        channel_messages[channel] = await recyle_messages(channel)
 
-
+    
+    bot.loop.create_task(updater())
 
 bot.run(cf.token)
 
