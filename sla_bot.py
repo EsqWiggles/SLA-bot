4e97c9c317fc8f640dcb19c066738b2f80b89116
Wiggly <esqwiggles@gmail.com>
2017-03-11 00:02:57 -0800

    Implement basic "next" and "find" commands for EQ schedule

--- sla_bot.py
+++ sla_bot.py
@@ -18,6 +18,7 @@ import SLA_bot.util as ut
 from   SLA_bot.alertfeed import AlertFeed
 from   SLA_bot.clock import Clock
 from   SLA_bot.channelupdater import ChannelUpdater
+from   SLA_bot.pso2calendar import PSO2Calendar
 
 VERSION = 0.17
 
@@ -112,7 +113,63 @@ bot = commands.Bot(command_prefix=cf.cmd_prefix, pm_help = True,
     # msg = '{} = {} - {}\n{}'.format(timezone, area, country, dt_line)
     
     # await ut.quiet_say(bot, msg, cf.max_line)
+
     
+def strfevent(event, ref_time):
+    td = '{:>7}'.format(ut.two_unit_tdelta(event.start - ref_time))
+    s = event.start.astimezone(cf.tz).strftime('%b %d, %H:%M')
+    e = event.end.astimezone(cf.tz).strftime('%H:%M %Z')
+    return '`|{:^9}|` **{}** @ {} ~ {}'.format(td, event.name, s, e)
+
+@bot.command()
+async def find(search=''):
+    if not search:
+        await boy.say('Please enter a name or part of name to search.')
+        return
+
+    now = dt.datetime.now(dt.timezone.utc)   
+    found = []
+    for event in PSO2Calendar.events:
+        if search.lower() in event.name.lower():
+            found.append(event)
+    if found:
+        lines = []
+        for name, count in PSO2Calendar.counter.items():
+            if search.lower() in name.lower():
+                lines.append('**{}** {}'.format(count, name))
+        lines.append('')
+        lines.extend([strfevent(event, now) for event in found])
+        msg = '\n'.join(lines)
+    else:
+        msg = 'No scheduled "{}" found.'.format(search) 
+    await bot.say(msg[:2000])
+
+
+    
+@bot.command(help = cs.NEXT_HELP)
+async def next(search='',):
+    if not PSO2Calendar.events:
+        await bot.say('No scheduled events remaining.')
+        return
+
+    now = dt.datetime.now(dt.timezone.utc)          
+    for event in PSO2Calendar.events:
+        if event.start > now:
+            next = event
+            break
+    
+    neighbor_time = dt.timedelta(hours=1)
+    if next:
+        found = [next]
+        later = [e for e in PSO2Calendar.events if e.start >= next.start]
+        for event in later:
+            if event != next and event.start - next.start <= neighbor_time:
+                found.append(event)
+        lines = [strfevent(event, now) for event in found]
+        msg = '\n'.join(lines)
+    await bot.say(msg[:2000])
+        
+
 @bot.event
 async def on_ready():
     print('Logged in as: {}'.format(bot.user.name))
