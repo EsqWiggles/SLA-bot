15f09d79511cbbc3639896dc18876c21727a622d
Wiggly <esqwiggles@gmail.com>
2017-03-11 00:28:14 -0800

    Re-enable status clock and delete unused code in sla_bot.py

--- sla_bot.py
+++ sla_bot.py
@@ -1,24 +1,23 @@
 import asyncio
-import collections
 import datetime as dt
-import math
 import os
 import sys
 import traceback
 
 import discord
 from   discord.ext import commands
-import pytz
 
-from   SLA_bot.alertsystem import AlertSystem
+from   SLA_bot.alertfeed import AlertFeed
+from   SLA_bot.channelupdater import ChannelUpdater
+from   SLA_bot.clock import Clock
 from   SLA_bot.config import Config as cf
 import SLA_bot.constants as cs
-from   SLA_bot.schedule import Schedule
-import SLA_bot.util as ut
-from   SLA_bot.alertfeed import AlertFeed
-from   SLA_bot.clock import Clock
-from   SLA_bot.channelupdater import ChannelUpdater
 from   SLA_bot.pso2calendar import PSO2Calendar
+import SLA_bot.util as ut
+
+
+
+
 
 VERSION = 0.17
 
@@ -35,85 +34,18 @@ cf.load_config(configs)
 bot = commands.Bot(command_prefix=cf.cmd_prefix, pm_help = True,
                    description=cs.BOT_HELP)
 
-# event_schedule = Schedule(bot)
-# bot.add_cog(event_schedule)
 
-# async def update_schedule():
-    # while not bot.is_closed:
-        # try:
-            # await event_schedule.update()
-        # except Exception:
-            # print('Ignored following error:')
-            # print(traceback.format_exc(), file=sys.stderr)
-        # await asyncio.sleep(cf.refresh_time)
-
-# bot.loop.create_task(update_schedule())
-
-# def country_zone():
-    # original = pytz.country_timezones
-    # inverted = {}
-    # for k, v in original.items():
-        # for zone in v:
-            # inverted[zone] = pytz.country_names[k]
-    # return inverted
-    
-# async def clock(bot):
-    # while not bot.is_closed:
-        # now = dt.datetime.now(dt.timezone.utc)
-        # try:
-            # time = now.astimezone(cf.tz).strftime('%H:%M %Z')
-            # help = 'Type {}help'.format(cf.cmd_prefix)
-            # status = '{} - {}'.format(time, help)
-            # await bot.change_status(game=discord.Game(name=status))
-        # except Exception:
-            # print('Ignored following error:')
-            # print(traceback.format_exc(), file=sys.stderr)
-        # await asyncio.sleep(60 - now.second)
-
-# @bot.command(help = cs.TZLIST_HELP)
-# async def tzlist(mode='other'):
-    # countries = collections.OrderedDict()
-    # others = collections.OrderedDict()
-    # country_codes = {x.lower() for x in pytz.country_timezones.keys()} 
-    # cc_from_tz = country_zone()
-    # for k, v in cf.custom_tz.items():
-        # if k in country_codes:
-            # countries[k] = v
-        # else:
-            # others[k] = v
-
-    # if mode == 'other':
-        # selected = others
-    # elif mode == 'country':
-        # selected = countries
-    # else:
-        # selected = collections.OrderedDict()
-
-    # lines = []
-    # for k, v in selected.items():
-        # country = cc_from_tz[v] if v in cc_from_tz else 'No Country'
-        # line = '{} = {} - {}'.format(k, v, country)
-        # lines.append(line)
-    # msg = ut.chunk_string('\n'.join(lines), cs.MSG_CHAR_LIMIT)
-    # await ut.quiet_say(bot, msg, cf.max_line)
-
-# @bot.command(help = cs.TZINFO_HELP)
-# async def tzinfo(timezone=''):
-    # tz = ut.parse_tz(timezone, cf.tz, cf.custom_tz)
-    
-    # country = 'No Country'
-    # area = getattr(tz, 'zone', 'Custom time zone')
-    # if hasattr(tz, 'zone'):
-        # for k, v in pytz.country_timezones.items():
-            # if tz.zone in v:
-                # country = pytz.country_names[k]
-                # break
-    
-    # dt_line = dt.datetime.now(tz).strftime('%a %b %m, %H:%M %Z (UTC%z)')
-    # msg = '{} = {} - {}\n{}'.format(timezone, area, country, dt_line)
-    
-    # await ut.quiet_say(bot, msg, cf.max_line)
-
+async def bot_status(bot):
+    while not bot.is_closed:
+        now = dt.datetime.now(dt.timezone.utc)
+        try:
+            time = now.astimezone(cf.tz).strftime('%H:%M %Z')
+            status = '{} - {}help'.format(time, cf.cmd_prefix)
+            await bot.change_presence(game=discord.Game(name=status))
+        except Exception:
+            print('Ignored following error:')
+            print(traceback.format_exc(), file=sys.stderr)
+        await asyncio.sleep(60 - now.second)
     
 def strfevent(event, ref_time):
     td = '{:>7}'.format(ut.two_unit_tdelta(event.start - ref_time))
@@ -144,8 +76,6 @@ async def find(search=''):
         msg = 'No scheduled "{}" found.'.format(search) 
     await bot.say(msg[:2000])
 
-
-    
 @bot.command(help = cs.NEXT_HELP)
 async def next(search='',):
     if not PSO2Calendar.events:
@@ -176,6 +106,7 @@ async def on_ready():
     print('------')
     updater = ChannelUpdater(bot)
     await updater.make_updaters()
+    bot.loop.create_task(bot_status(bot))
 
 
 bot.run(cf.token)
