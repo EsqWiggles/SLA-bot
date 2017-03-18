0f7de2be5ced58a2e65b201b0fb6e7c08862f7c2
Wiggly <esqwiggles@gmail.com>
2017-03-18 12:37:34 -0700

    Connect module refresh time to their config option

--- SLA_bot/channelupdater.py
+++ SLA_bot/channelupdater.py
@@ -69,6 +69,9 @@ class ChannelUpdater:
             
     async def make_updaters(self):
         await self.load_channels()
-        self.bot.loop.create_task(self.updater(Clock.fetch, 0, 2))
-        self.bot.loop.create_task(self.updater(AlertFeed.fetch, 1, 8))
-        self.bot.loop.create_task(self.updater(PSO2Calendar.fetch, 2, 8))
+        clock_i = cf.getint('Clock', 'update_interval')
+        feed_i = cf.getint('PSO2 Feed', 'update_interval')
+        cal_i = cf.getint('PSO2 Calendar', 'update_interval')
+        self.bot.loop.create_task(self.updater(Clock.fetch, 0, clock_i))
+        self.bot.loop.create_task(self.updater(AlertFeed.fetch, 1, feed_i))
+        self.bot.loop.create_task(self.updater(PSO2Calendar.fetch, 2, cal_i))
