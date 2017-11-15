589316a0d0634e47ff1cbb22d818cc3cd44ed0c6
Wiggly <esqwiggles@gmail.com>
2017-11-15 02:03:19 -0800

    Read pso2 summary aliases from config

--- SLA_bot/module/pso2summary.py
+++ SLA_bot/module/pso2summary.py
@@ -1,11 +1,25 @@
 import asyncio
+import collections
 import datetime as dt
 import string
 
+import SLA_bot.config as cf
 import SLA_bot.module.pso2calendar as PSO2Calendar
 
-groups = ["Arks League", "Casino Boost"]
+
 transtable = str.maketrans("", "", string.punctuation + string.whitespace)
+def strip(s):
+    return s.translate(transtable).lower()
+    
+search_alias = collections.OrderedDict()
+for substring, alias in cf.section('PSO2 Summary').items():
+    search_alias[strip(substring)] = alias
+name_countlabel = {}
+
+class Counter:
+    def __init__(self, name, count):
+        self.name = name
+        self.count = count
 
 def read():
     events = PSO2Calendar.events
@@ -15,34 +29,36 @@ def read():
 
 def strfevents(events):
     lines = []
-    name_count = count_events(events)
-    count_length = len(str(max(name_count.values())))
-    data = sorted(name_count.items(), key=lambda x: x[0])
-    for name, count in data:
-        lines.append('`x {:{width}d}` {}'.format(count, name, width=count_length))
+    counters = list(count_events(events).values())
+    count_length = len(str(max( [x.count for x in counters] )))
+    data = sorted(counters, key=lambda x: x.name)
+    for c in data:
+        lines.append('`x {:{width}d}` {}'.format(c.count, c.name, width=count_length))
     return '\n'.join(lines)
-    
+
 def count_events(events):
     count = {}
-    earliest_name = {}
     for e in events:
-        key = e.name
-        stripped = strip(key)
-        for g in groups:
-            if strip(g) in stripped:
-                key = g
-        if stripped in earliest_name:
-            key = earliest_name[stripped]
-
-        try:
-            count[key] += 1
-        except KeyError:
-            count[key] = 1
-            earliest_name[stripped] = key
+        name = e.name
+        label = find_label(name)
+        if label in count:
+            count[label].count += 1
+        else:
+            counter_name = name
+            for alias in search_alias.values():
+                if label == strip(alias):
+                    counter_name = alias
+            count[label] = Counter(counter_name, 1)
     return count
-
-def strip(s):
-    return s.translate(transtable).lower()
     
-
-    
+def find_label(name):
+    global name_countlabel
+    if name in name_countlabel:
+        return name_countlabel[name]
+    stripped = strip(name)
+    for search, alias in search_alias.items():
+        if search in stripped:
+            name_countlabel[name] = strip(alias)
+            return name_countlabel[name]
+    name_countlabel[name] = stripped
+    return stripped
