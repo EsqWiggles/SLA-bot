e9ec148ca736fd8893d619230dade78ecf096803
Wiggly <esqwiggles@gmail.com>
2017-11-24 05:51:22 -0800

    Simplify pso2summary counting

--- SLA_bot/module/pso2summary.py
+++ SLA_bot/module/pso2summary.py
@@ -1,66 +1,58 @@
 import asyncio
 import collections
+import copy
 import datetime as dt
 import string
 
 import SLA_bot.config as cf
 import SLA_bot.module.pso2calendar as PSO2Calendar
 
+cache = ''
+counted_events = []
 
 transtable = str.maketrans("", "", string.punctuation + string.whitespace)
 def strip(s):
     return s.translate(transtable).lower()
-    
+
 search_alias = collections.OrderedDict()
 for substring, alias in cf.section('PSO2 Summary').items():
     search_alias[strip(substring)] = alias
-name_countlabel = {}
-
-class Counter:
-    def __init__(self, name, count):
-        self.name = name
-        self.count = count
 
 def read():
-    events = PSO2Calendar.events
+    now = dt.datetime.now(dt.timezone.utc)
+    events = [x for x in PSO2Calendar.events if x.end > now]
     if not events:
         return '`x 0` Scheduled events!'
-    return strfevents(events) + '\n** **'
+    
+    global cache
+    global counted_events
+    if counted_events != events or not cache:
+        counted_events = copy.copy(events)
+        cache = strfcount(count_events(events)) + '\n** **'
+    return cache
 
-def strfevents(events):
+def strfcount(name_count):
     lines = []
-    counters = list(count_events(events).values())
-    count_length = len(str(max( [x.count for x in counters] )))
-    data = sorted(counters, key=lambda x: x.name)
-    for c in data:
-        lines.append('`x {:{width}d}` {}'.format(c.count, c.name, width=count_length))
+    c_width = len(str(max(name_count.values())))
+    data = sorted(name_count.items(), key=lambda x: x[0])
+    for name, count in data:
+        lines.append('`x {:{width}d}` {}'.format(count, name, width=c_width))
     return '\n'.join(lines)
 
 def count_events(events):
-    count = {}
-    for alias in search_alias.values():
-        count[strip(alias)] = Counter(alias, 0)
+    label_firstname = {}
+    name_count = {}
     for e in events:
         name = e.name
-        label = find_label(name)
-        if label in count:
-            count[label].count += 1
+        label = strip(e.name)
+        if label in label_firstname:
+            name_count[label_firstname[label]] += 1
         else:
-            count[label] = Counter(name, 1)  
-    for alias in search_alias.values():
-        label = strip(alias)
-        if label in count and count[label].count <= 0:
-            del count[label]
-    return count
-    
-def find_label(name):
-    global name_countlabel
-    if name in name_countlabel:
-        return name_countlabel[name]
-    stripped = strip(name)
-    for search, alias in search_alias.items():
-        if search in stripped:
-            name_countlabel[name] = strip(alias)
-            return name_countlabel[name]
-    name_countlabel[name] = stripped
-    return stripped
+            for search, alias in search_alias.items():
+                if search in label:
+                    name = alias
+                    break
+            label_firstname[label] = name
+            name_count.setdefault(name, 0)
+            name_count[name] += 1
+    return name_count
