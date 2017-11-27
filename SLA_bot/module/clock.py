e09c4cf02c7841ae71aa586f24a9b65feda1a375
Wiggly <esqwiggles@gmail.com>
2017-11-26 18:27:41 -0800

    Document clock.py

--- SLA_bot/module/clock.py
+++ SLA_bot/module/clock.py
@@ -1,15 +1,18 @@
-import datetime as dt
+"""Simple datetime output with a semi-animated clock."""
 
+import datetime as dt
 import SLA_bot.config as cf
 
 hand = 0
 tz = cf.gettimezone('General', 'timezone')
 
 def update():
+    """Move clock hand forward by 1 hour."""
     global hand
     hand = hand % 12 + 1
 
 def read():
+    """Return string of current time and date with a clock icon."""
     time = dt.datetime.now(tz).strftime('%H:%M %Z - %m/%d')
     return ':clock{}: {}'.format(hand, time)
     
