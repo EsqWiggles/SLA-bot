9a0a2ba3311cf233c31587489f4c4a928626c7c6
Wiggly <esqwiggles@gmail.com>
2017-12-04 01:21:28 -0800

    Rearrange function order in channelupdater.py

--- SLA_bot/channelupdater.py
+++ SLA_bot/channelupdater.py
@@ -29,6 +29,27 @@ async def init(discord_bot):
     global bot
     bot = discord_bot
     await make_updaters()
+        
+async def make_updaters():
+    """Create and load the module and channel updaters into the bot loop."""
+    await load_channels()
+    update_delays = {
+        AlertFeed.update : cf.getint('PSO2 Feed', 'update_interval'),
+        bumpedRSS.update : cf.getint('bumped RSS', 'update_interval'),
+        PSO2Calendar.update : cf.getint('PSO2 Calendar', 'update_interval'),
+        PSO2esCalendar.update : cf.getint('PSO2es Calendar', 'update_interval'),
+        PSO2RSS.update : cf.getint('PSO2 RSS', 'update_interval')}
+    for func, delay in update_delays.items():
+        bot.loop.create_task(updater(func, delay))
+    bot.loop.create_task(update_messages(cf.getint('General', 'channel_update_interval')))
+
+async def load_channels():
+    """Load the channel IDs from config."""
+    global channel_messages
+    channel_messages = {}
+    for c in cf.channels():
+        chan = bot.get_channel(c)
+        channel_messages[chan] = await recycle_messages(chan)
 
 async def recycle_messages(channel):
     """Return the last message sent by this bot in the channel.
@@ -50,6 +71,49 @@ async def recycle_messages(channel):
         return None
     return None
 
+async def updater(updateFunc, interval):
+    """Create an update loop to call updateFunc every interval seconds."""
+    while not bot.is_closed:
+        try:
+            await updateFunc()
+        except asyncio.CancelledError:
+            break
+        except:
+            ut.print_new_exceptions()
+        await asyncio.sleep(interval)
+
+async def update_messages(interval):
+    """Start Loop to send newly built message every interval seconds."""
+    while not bot.is_closed:
+        try:
+            Clock.update()
+            content, embed = await build_message()
+            for channel, messages in channel_messages.items():
+                await write_content(channel, content, embed)
+        except asyncio.CancelledError:
+            break
+        except:
+            ut.print_new_exceptions()
+        await asyncio.sleep(interval)
+
+async def build_message():
+    """Return the compiled text of the modules as a content and embed tuple."""
+    content = None
+    embed=discord.Embed(title=Clock.read(), description='** **',  color=color)
+    alert_header, alert_body = AlertFeed.read().split('\n', maxsplit=1)
+    if AlertFeed.is_unscheduled():
+        embed.add_field(name=alert_header, value=alert_body, inline=True)
+    if PSO2Calendar.events:
+        embed.add_field(name='**PSO2 Schedule**', value=PSO2Calendar.read(), inline=True)
+        embed.add_field(name='**PSO2 Summary**', value=PSO2Summary.read(), inline=True)
+    if PSO2esCalendar.events:
+        embed.add_field(name='**PSO2es**', value=PSO2esCalendar.read(), inline=True)
+    embed.add_field(name='bumped.org', value=bumpedRSS.read(), inline=True)
+    if not AlertFeed.is_unscheduled():
+        embed.add_field(name=alert_header, value=alert_body, inline=True)
+    embed.add_field(name='pso2.jp/players/...', value=PSO2RSS.read(), inline=False)
+    return (content, embed)
+        
 async def write_content(channel, content, embed):
     """Send the content with the embed into the channel.
     
@@ -73,67 +137,3 @@ async def write_content(channel, content, embed):
     except discord.errors.Forbidden:
         pass
 
-async def build_message():
-    """Return the compiled text of the modules as a content and embed tuple."""
-    content = None
-    embed=discord.Embed(title=Clock.read(), description='** **',  color=color)
-    alert_header, alert_body = AlertFeed.read().split('\n', maxsplit=1)
-    if AlertFeed.is_unscheduled():
-        embed.add_field(name=alert_header, value=alert_body, inline=True)
-    if PSO2Calendar.events:
-        embed.add_field(name='**PSO2 Schedule**', value=PSO2Calendar.read(), inline=True)
-        embed.add_field(name='**PSO2 Summary**', value=PSO2Summary.read(), inline=True)
-    if PSO2esCalendar.events:
-        embed.add_field(name='**PSO2es**', value=PSO2esCalendar.read(), inline=True)
-    embed.add_field(name='bumped.org', value=bumpedRSS.read(), inline=True)
-    if not AlertFeed.is_unscheduled():
-        embed.add_field(name=alert_header, value=alert_body, inline=True)
-    embed.add_field(name='pso2.jp/players/...', value=PSO2RSS.read(), inline=False)
-    return (content, embed)
-
-        
-async def update_messages(interval):
-    """Start Loop to send newly built message every interval seconds."""
-    while not bot.is_closed:
-        try:
-            Clock.update()
-            content, embed = await build_message()
-            for channel, messages in channel_messages.items():
-                await write_content(channel, content, embed)
-        except asyncio.CancelledError:
-            break
-        except:
-            ut.print_new_exceptions()
-        await asyncio.sleep(interval)
-    
-async def load_channels():
-    """Load the channel IDs from config."""
-    global channel_messages
-    channel_messages = {}
-    for c in cf.channels():
-        chan = bot.get_channel(c)
-        channel_messages[chan] = await recycle_messages(chan)
-
-async def updater(updateFunc, interval):
-    """Create an update loop to call updateFunc every interval seconds."""
-    while not bot.is_closed:
-        try:
-            await updateFunc()
-        except asyncio.CancelledError:
-            break
-        except:
-            ut.print_new_exceptions()
-        await asyncio.sleep(interval)
-        
-async def make_updaters():
-    """Create and load the module and channel updaters into the bot loop."""
-    await load_channels()
-    update_delays = {
-        AlertFeed.update : cf.getint('PSO2 Feed', 'update_interval'),
-        bumpedRSS.update : cf.getint('bumped RSS', 'update_interval'),
-        PSO2Calendar.update : cf.getint('PSO2 Calendar', 'update_interval'),
-        PSO2esCalendar.update : cf.getint('PSO2es Calendar', 'update_interval'),
-        PSO2RSS.update : cf.getint('PSO2 RSS', 'update_interval')}
-    for func, delay in update_delays.items():
-        bot.loop.create_task(updater(func, delay))
-    bot.loop.create_task(update_messages(cf.getint('General', 'channel_update_interval')))
