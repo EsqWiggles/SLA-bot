ff31104d15a2dc9e0b7b97c19804a7f6a4287c0d
Wiggly <esqwiggles@gmail.com>
2017-12-07 20:49:18 -0800

    Document sla_bot.py

--- sla_bot.py
+++ sla_bot.py
@@ -1,3 +1,14 @@
+"""Run Discord bot to aggregate supplementary resources for PSO2
+
+Example:
+    This bot must be run on python 3.5 or later. Depending on your system,
+    python, python3, or python3.5 should be used. Because the bot is
+    generally left on for long periods of time, you may want to save the
+    stderr to a file to check for errors.
+        $ python sla_bot.py
+        $ python3 sla_bot.py 
+        $ python3 sla_bot.py 2>> error.log
+"""
 import asyncio
 import datetime as dt
 import os
@@ -5,7 +16,7 @@ import os
 import discord
 from   discord.ext import commands
 
-
+#Load config first so other project files can save config vars at module level
 import SLA_bot.config as cf
 curr_dir = os.path.dirname(__file__)
 default_config = os.path.join(curr_dir, 'default_config.ini'),
@@ -33,7 +44,8 @@ initialized = False
 bot = commands.Bot(command_prefix=prefix, pm_help=True, description=cs.BOT_HELP)
 bot.add_cog(UserCommands(bot))
 
-async def bot_status(bot):        
+async def bot_status(bot):
+    """Run a clock on the bot's status line."""
     while not bot.is_closed:
         try:
             now = dt.datetime.now(dt.timezone.utc)
@@ -49,6 +61,8 @@ async def bot_status(bot):
 async def on_ready():
     print('Logged in as: {}'.format(bot.user.name))
     print('------')
+    
+    #This method may be called more than once
     global initialized
     if not initialized:
         await ChannelUpdater.init(bot)
@@ -57,5 +71,3 @@ async def on_ready():
 
 
 bot.run(cf.get('General', 'bot_token'))
-
-
