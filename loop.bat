2ffb6338f05b025b2d0d7e60f4bedadf249df22b
Wiggly <esqwiggles@gmail.com>
2019-01-25 08:20:07 -0800

    Simplify editing the looping scripts

--- loop.bat
+++ loop.bat
@@ -1,6 +1,10 @@
 @echo off
+rem edit python_call and bot_path as needed
+set python_call=python
+set bot_path=sla_bot.py
+
 for /l %%x in (1, 0, 2) do (
     echo ================ Restarting ================
-    python sla_bot.py
+    %python_call% %bot_path%
     timeout /t 20
 )
--- loop.sh
+++ loop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #screen -d -m this
+#edit python_call and bot_path as needed
+python_call=python3
+bot_path=sla_bot.py
 
 while true; do
-   python3.5 SLA-bot/sla_bot.py
+   $python_call $bot_path
    read -t 10 -p $'Retrying... press ENTER within 10 seconds to stop.\n'
    if [ $? == 0 ]; then
       break
